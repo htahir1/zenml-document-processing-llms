124291a90c5a1eaed4ce821c17054aef1c51578b
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-02-13 18:04:10 +0100

    add colpali rag

--- test.py
+++ test.py
@@ -1,39 +1,31 @@
 from byaldi import RAGMultiModalModel
 from rich import print
-from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
-from qwen_vl_utils import process_vision_info
+from transformers import MllamaForConditionalGeneration, AutoProcessor
 import torch
 from pdf2image import convert_from_path
 
 
 def main():
-    images = convert_from_path("data/microsoft-q2-2025-small.pdf")
+    images = convert_from_path("data/microsoft-q2-2025-full.pdf")
 
     RAG = RAGMultiModalModel.from_pretrained("vidore/colpali")
     RAG.index(
-        input_path="data/microsoft-q2-2025-small.pdf",
-        index_name="image_index",  # index will be saved at index_root/index_name/
+        input_path="data/microsoft-q2-2025-full.pdf",
+        index_name="image_index_full",  # index will be saved at index_root/index_name/
         store_collection_with_index=False,
         overwrite=True,
     )
-    text_query = "What was Microsoft's net income in Q2 2025?"
-    results = RAG.search(text_query, k=1)
+    text_query = "How is Microsoft doing, financially?"
+    results = RAG.search(text_query, k=3)
 
-    # reset the CUDA cache
-    torch.cuda.empty_cache()
-
-    model = (
-        Qwen2VLForConditionalGeneration.from_pretrained(
-            "Qwen/Qwen2-VL-2B-Instruct",
-            trust_remote_code=True,
-            torch_dtype=torch.bfloat16,
-        )
-        .cuda()
-        .eval()
+    model = MllamaForConditionalGeneration.from_pretrained(
+        "meta-llama/Llama-3.2-11B-Vision-Instruct",
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
     )
 
     processor = AutoProcessor.from_pretrained(
-        "Qwen/Qwen2-VL-2B-Instruct", trust_remote_code=True
+        "meta-llama/Llama-3.2-11B-Vision-Instruct"
     )
 
     image_index = results[0]["page_num"] - 1
@@ -43,38 +35,32 @@ def main():
             "content": [
                 {
                     "type": "image",
-                    "image": images[image_index],
                 },
                 {"type": "text", "text": text_query},
             ],
         }
     ]
 
-    text = processor.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True
-    )
-
-    image_inputs, video_inputs = process_vision_info(messages)
+    input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
     inputs = processor(
-        text=[text],
-        images=image_inputs,
-        videos=video_inputs,
-        padding=True,
+        images=images[image_index],
+        text=input_text,
+        add_special_tokens=False,
         return_tensors="pt",
-    )
-    inputs = inputs.to("cuda")
+    ).to(model.device)
 
-    generated_ids = model.generate(**inputs, max_new_tokens=50)
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :]
-        for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    output = model.generate(**inputs, max_new_tokens=500)
+    output_text = processor.decode(output[0])
+
+    output_trimmed = [
+        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, output)
     ]
+
     output_text = processor.batch_decode(
-        generated_ids_trimmed,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False,
+        output_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
     )
-    print(output_text)
+
+    print(output_text[0])
 
 
 if __name__ == "__main__":
